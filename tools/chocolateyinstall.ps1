2aafb727cb8a92dd500219a3e24097ae41351554
Yatao Li <yatao.li@live.com>
2016-07-30 23:54:26 +0800

    initial commit of the rest of the package.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'wechat'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://dlglobal.qq.com/weixin/Windows/WeChat_C1018.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+
+  softwareName  = 'WeChat*'
+  checksum      = 'a4c730850d97eced773a4c47e0917a63'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
