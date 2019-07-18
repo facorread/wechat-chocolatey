8fc130cef9e2ca05d561827541031fd30848be62
Fabio Correa <facorread@gmail.com>
2019-07-18 15:22:10 -0400

    Revert "Using the automatic uninstaller"
    This reverts commit 3b9b89a99e83460db8e93dc77a5fbf7c12041835.

---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop';
+$installerType = 'EXE' 
+$silentArgs = '/qn /norestart'
+$validExitCodes = @(0, 3010, 1605, 1614, 1641)
+
+if ($installerType -ne 'MSI') {
+  $silentArgs = '/S'
+  $validExitCodes = @(0)
+}
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName 'WeChat*'
+
+if ($key.Count -eq 1) {
+  $key | % { 
+    $file = "$($_.UninstallString)"
+
+    if ($installerType -eq 'MSI') {
+      $silentArgs = "$($_.PSChildName) $silentArgs"
+      $file = ''
+    }
+
+    Uninstall-ChocolateyPackage -PackageName wechat `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "We did not find a WeChat installation."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "We found $key.Count WeChat installations!"
+  Write-Warning "We will stop here to prevent accidental data loss."
+  Write-Warning "Please alert the maintainer of this Chocolatey package about the following keys:"
+  $key | % {Write-Warning "- $_.DisplayName"}
+}
