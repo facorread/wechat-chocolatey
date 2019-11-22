94de00ce78d6270228706e8143c79a7011e68c39
冰晶 <601519305@qq.com>
2019-11-21 20:26:47 -0800

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName   = 'wechat'
   fileType      = 'exe'
-  url           = 'https://dlglobal.qq.com/weixin/Windows/WeChat_C1018.exe'
+  url           = 'http://dlglobal.qq.com/weixin/Windows/WeChat_C1018.exe'
   silentArgs   = '/S'
   softwareName  = 'WeChat*'
   checksum      = '750b36b615625a9fb5821ba61616da860ed0925012d7b7aad81861d2176fdfb7b3cfa2d59c6f98ee9e732fa8ee3ca9cff3df137a2e47b38c7f706d7c8d4813c5'
