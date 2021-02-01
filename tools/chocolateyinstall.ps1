cc9520bcdb97dfc2bd66f398b656fe05ae006f3c
Fabio A. Correa Duran <facorread@gmail.com>
2021-02-01 10:03:57 -0500

    3.1.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   url          = 'https://dldir1.qq.com/weixin/Windows/WeChatSetup.exe'
   silentArgs   = '/S'
   softwareName = 'WeChat*'
-  checksum     = '95ab26c3048b6705954549d30bb4f3ee12c8a61c3a961477fa7d736d2792ffc01f6e550472b8c0148d7df66a04c9877b5a4857dc56684b96ea0baa56b184d74b'
+  checksum     = 'BD45B8A549F24E4D98B628D2B1EB6C4E65B210FAF5015232F1A6033B4033C50479F1FC8733461BD9A2821AA21CA1B4ECCCA37D3ACA4F6FCDEFDDAB2AAC3E91DC'
   checksumType = 'sha512'
 }
 
